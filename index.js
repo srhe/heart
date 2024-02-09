277cbea8447a795e9ed85fc1592fc679bcce1af8
srhe <1604086129@qq.com>
2024-02-09 16:54:04 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+  (function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?c923daf3182a4b0ce01878475080aadc";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+  })();
